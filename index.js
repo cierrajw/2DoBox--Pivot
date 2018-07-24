3a04bd015fc0551966d0ad818d62f2e395b7dbb3
Cierra Washington <cierrajwashi@gmail.com>
2018-07-24 15:57:05 -0600

    add delete from local storage function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-$(window).on('load', getItems);
+$(window).on('load', getLocalStorage);
 //Event Listeners
 $('.save-btn').on('click', submitToDo);
 
@@ -8,7 +8,7 @@ $('.card-section').on('click', checkTarget);
 
 //Functions
 
-function getItems(){
+function getLocalStorage(){
 
     for(let i=0; i < localStorage.length; i++){
 
@@ -51,15 +51,6 @@ function NewToDo(){
     this.task =  $('.task-input').val();
     this.id = Date.now();
     this.currentImportance = "Normal";
-
-
-    // return {
-    //     title: $('.title-input').val(),
-    //     body: $('.body-input').val(),
-    //     id: Date.now(),
-    //     currentImportance: "Normal",
-    //     }
-
 }
 
 function submitToDo(event){
@@ -102,9 +93,9 @@ function setLocalStorage(NewToDo){
 function newToDoCard(task){
     // console.log("This:" + Object.keys(this));
     var newCard = `<article class="posted-card" data-id="${task.id}">
-                    <h2 class="title-of-card">${task.title}</h2>
+                    <h2 class="title-of-card" contenteditable="true">${task.title}</h2>
                     <button id="deletebutton" class="delete-button card-buttons"></button>
-                    <p class="card-text">${task.task}</p>
+                    <p class="card-text" contenteditable="true">${task.task}</p>
                     <button id="upvotebutton" class="upvote card-buttons"></button>
                     <button id="downvotebutton" class="downvote card-buttons"></button>
                     <p class="quality card-text">Quality: <span>${task.currentImportance}</span></p>
@@ -138,105 +129,14 @@ function checkTarget(event){
 
 function deleteCard(event){
 
-    var card = $(event.target).closest('article').remove();
+    var card = $(event.target).closest('article');
 
-    localStorage.removeItem('todo');
+    card.remove();
 
-    if(event.target.className === 'delete-button'){
-        alert("Hey u clicked a DELETE button!");
-    }
-    console.log("event target:" + event.target.className === 'delete-button');
+    var deleteId = card.prop('dataset').id;
 
-     // alert("Hey u clicked the CARD SECTION!");
-    // ($event.target).className == ""
-
-    // localStorage.remove
+    localStorage.removeItem(deleteId);
 }
-
-// get local storage of new card
-// function getLocalStorage(cardSerialized){
-
-//     var getCard = JSON.parse(localStorage.getItem(cardSerialized));
-//     console.log(getCard);
-//     // newToDoCard(getCard.title);
-// }
-
-
-
-
-
-
-
-// function parseLocalStorage(cardData){
-
-// }
-
-//parse stored card to get it
-// function parseLocalStorage(){
-
-// }
-
-
-    // numCards++;
-
-//     $( ".card-section" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
-// });
-
-// var localStoreCard = function() {
-//     var cardString = JSON.stringify(cardObject());
-//     localStorage.setItem('card' + numCards  , cardString);
-// }
-
-// $('.save-btn').on('click', function(event) {
-//     event.preventDefault();
-//     if ($('#title-input').val() === "" || $('#body-input').val() === "") {
-//        return false;
-//     };  
-
-//     numCards++;
-//     $( ".card-section" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
-//     localStoreCard();
-//     $('form')[0].reset();
-// });
-
-
-
-
-//Alex up/downvote buttons
-
-
-// on.("click", qualityArray())
-
-// function qualityArray() {
-//     var qualities = ["swill", "plausible", "genius"];
-//     upVote(qualities);
-//     downVote(qualities);
-// };
-
-// function upVote(quality) {
-//     console.log(quality[0], quality[1], quality[2]);
-//     Once function is run, will iterate once.  ex. quality++, each time 
-//     Take whatever quality is on page, compare it to whatever value you pass through,
-//     then iterate through array to --
-// }
-
-// function qualityVariables() {
-//     var currentQuality = ; $
-// }
-
-// function upVotePlausible() {
-//     if(event.target.className === "upvote" && )
-// };
-
-
-// Add array index[i] to 'p.quality';
-
-
-//Event listener on .card-section to allow for upvoting, downvoting 
-
-//NEW CODE
-
-// $("card-section")on.("click", qualityArray);
 
 function qualityArray() {
     var qualities = ["swill", "plausible", "genius"];
@@ -262,52 +162,20 @@ function downVote(event) {
 };
 
 
+    // numCards++;
 
-//OLD CODE
+//     $( ".card-section" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
+// });
 
-// $(".card-section").on('click', function(event){
-//     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-//     var qualityVariable;
 
-//     if (event.target.className === "upvote" || event.target.className === "downvote"){
+// $('.save-btn').on('click', function(event) {
+//     event.preventDefault();
+//     if ($('#title-input').val() === "" || $('#body-input').val() === "") {
+//        return false;
+//     };  
 
-//         if (event.target.className === "upvote" && currentQuality === "plausible"){
-//             qualityVariable = "genius";
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-//         } else if (event.target.className === "upvote" && currentQuality === "swill") {
-//             qualityVariable = "plausible";
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-//         } else if (event.target.className === "downvote" && currentQuality === "plausible") {
-//             qualityVariable = "swill"
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-//         } else if (event.target.className === "downvote" && currentQuality === "genius") {
-//             qualityVariable = "plausible"
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-//         } else if (event.target.className === "downvote" && currentQuality === "swill") {
-//             qualityVariable = "swill";
-        
-//         } else if (event.target.className === "upvote" && currentQuality === "genius") {
-//             qualityVariable = "genius";
-//         }
-
-//     var cardHTML = $(event.target).closest('.card-container');
-//     var cardHTMLId = cardHTML[0].id;
-//     var cardObjectInJSON = localStorage.getItem(cardHTMLId);
-//     var cardObjectInJS = JSON.parse(cardObjectInJSON);
-
-//     cardObjectInJS.quality = qualityVariable;
-
-//     var newCardJSON = JSON.stringify(cardObjectInJS);
-//     localStorage.setItem(cardHTMLId, newCardJSON);
-//     }
-   
-//     else if (event.target.className === "delete-button") {
-//         var cardHTML = $(event.target).closest('.card-container').remove();
-//         var cardHTMLId = cardHTML[0].id;
-//         localStorage.removeItem(cardHTMLId);
-//     }
+//     numCards++;
+//     $( ".card-section" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
+//     localStoreCard();
+//     $('form')[0].reset();
 // });
