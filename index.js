6ebd09e5752e5e236c4feda4824b7dd7662bc2ad
Cierra Washington <cierrajwashi@gmail.com>
2018-07-25 15:11:25 -0600

    Add eddit feature

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ $('.save-btn').on('click', submitToDo);
 
 $('.card-section').on('click', checkTarget);
 
-// $('.card-section').on('blur', saveEdits);
-
 
 // jquery on event delegation, listen to specific items in the card, and use 'this'
 
@@ -25,27 +23,21 @@ function getLocalStorage(){
         
     newToDoCard(parsedItem);
 
-        console.log(parsedItem);
+    console.log(parsedItem);
     }
 }
 
 function NewToDo(){ 
 
+    this.id = Date.now();
     this.title = $('.title-input').val();
     this.task =  $('.task-input').val();
-    this.id = Date.now();
     this.currentImportance = "Normal";
 }
 
 
 function submitToDo(event){
     event.preventDefault();
-
-    // var cards = $('.card-section')[0].childNodes;
-
-    // if(cards){
-    //     $('.input').prop('disabled', false); 
-    // }
 
     var todo = new NewToDo();
 
@@ -73,18 +65,15 @@ function setLocalStorage(NewToDo){
 
     var cardSerialized = localStorage.setItem(cardKey, JSON.stringify(NewToDo));
 
-    console.log("This is the card key:" + cardSerialized);
-
     console.log("Set and stringified data: " + cardSerialized);
 
-    // getLocalStorage(cardSerialized);
 }
 
 function newToDoCard(task){
     // console.log("This:" + Object.keys(this));
     var newCard = `<article class="posted-card" data-id="${task.id}">
 
-                    <h2 class="title-of-card" contenteditable="true" data-title="${task.title}">${task.title}</h2>
+                    <h2 class="title-of-card" data-thetitle="${task.title}" contenteditable="true">${task.title}</h2>
                     <button id="deletebutton" class="delete-button card-buttons"></button>
                     <button class="edit-button">Edit</button>
                     <p class="card-task" contenteditable="true" data-task="${task.task}">${task.task}</p>
@@ -100,56 +89,68 @@ function newToDoCard(task){
 function checkTarget(event){
 
     if (event.target.id === 'deletebutton'){
-
         deleteCard(event);
     }
     else if(event.target.id === 'upvotebutton'){
-
         console.log("Hey u clicked the UPVOTE button!");
         storeUpVote(event);
     }
     else if(event.target.id === 'downvotebutton'){
-
         console.log("Hey u clicked the DOWNVOTE button!");
         downVote(event);
     }
-    else if(event.target.className === 'title-of-card' || 'card-task'){
-
-        $(this).on('keydown', saveEdits);
-
+    else if(event.target.className === 'title-of-card'){
+        $(this).on('keyup', saveEdits);
     }
 }
 
 
 function saveEdits(event){
 
-var title = $(event.target).closest('title-of-card');
+var title = $(event.target).closest('.title-of-card');
 
-// console.log("the thing: ", $(event.target).closest('title-of-card'));
+var titleID = title.parent();
 
+var titleInnerHTML = title[0].innerHTML;
 
-var tehtitle = title.prop('dataset').title;
+var retrieved2do = localStorage.getItem(titleID);
 
-console.log("The awesome title thing: ", tehtitle);
+parsedToDo = JSON.parse(retrieved2do);
 
-// console.log("The property trying to get: " , tehtitle);
-
-    console.log("saved edits!!!");
-    // var editButton = $(event.target).closest('edit-button');
-
-    // editButton.prop('contenteditable', 'true');
-     
-
-
+var editedIdea = {
+    title: titleInnerHTML,
+    id: Date.now()
 }
+
+var stringifiedToDo = JSON.stringify(editedIdea);
+
+localStorage.setItem(titleID, stringifiedToDo);
+
+// var originalData = title.prop('dataset').thetitle;
+
+
+//give edited title an id
+
+ // var newCardId = $(event.target).closest().attr('id');
+
+ // console.log(newCardId);
+
+
+
+console.log("The inner HTML: ", titleInnerHTML);
+
+     
+}
+
+
 
 function deleteCard(event){
 
     var card = $(event.target).closest('article');
 
-    card.remove();
+    console.log("The card object: ", card);
 
-    console.log("The REAL dataset:", card.prop('dataset'));
+    card.remove();
 
     var deleteId = card.prop('dataset').id;
 
@@ -159,34 +160,34 @@ function deleteCard(event){
 
 
 // Upvote
-function storeUpVote(event) {
-    var clickedArticle = $(event.target).closest(".posted-card");
-    var parsedObj = getNParse(clickedArticle.attr("data-id"));
-    upVote(parsedObj);
-    stringNSet(parsedObj);
-    clickedArticle.find("#importance").text(parsedObj.currentImportance);
-    console.log("This is theeee" + parsedObj)
-};
+// function storeUpVote(event) {
+//     var clickedArticle = $(event.target).closest(".posted-card");
+//     var parsedObj = getNParse(clickedArticle.attr("data-id"));
+//     upVote(parsedObj);
+//     stringNSet(parsedObj);
+//     clickedArticle.find("#importance").text(parsedObj.currentImportance);
+//     console.log("This is theeee" + parsedObj)
+// };
 
-function upVote(event) {
-    var qualities = ["None", "Low", "Normal", "High", "Critical"];
-    var currentQuality = $(event.target).nextAll("p").children().text();
-    for(var i = 0; i < qualities.length; i++) {
-        if(qualities[i] === currentQuality) {
-            $(event.target).nextAll("p").children().text(qualities[i + 1]);
-        };
-    console.log(currentQuality);
-    };
-};
+// function upVote(event) {
+//     var qualities = ["None", "Low", "Normal", "High", "Critical"];
+//     var currentQuality = $(event.target).nextAll("p").children().text();
+//     for(var i = 0; i < qualities.length; i++) {
+//         if(qualities[i] === currentQuality) {
+//             $(event.target).nextAll("p").children().text(qualities[i + 1]);
+//         };
+//     console.log(currentQuality);
+//     };
+// };
 
 // DownVote
-function downVote(event) {
-    var qualities = ["None", "Low", "Normal", "High", "Critical"];
-    var currentQuality = $(event.target).nextAll("p").children().text();
-    for(var i = 0; i < qualities.length; i++) {
-        if(qualities[i] === currentQuality) {
-            $(event.target).nextAll("p").children().text(qualities[i - 1]);
-        };
-    };
+// function downVote(event) {
+//     var qualities = ["None", "Low", "Normal", "High", "Critical"];
+//     var currentQuality = $(event.target).nextAll("p").children().text();
+//     for(var i = 0; i < qualities.length; i++) {
+//         if(qualities[i] === currentQuality) {
+//             $(event.target).nextAll("p").children().text(qualities[i - 1]);
+//         };
+//     };
 
-};
+// };
